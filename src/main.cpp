a24a766005791743b09ab7a42311f6e647784fd0
esmil <esmilviciosomercado@gmail.com>
2025-07-16 14:53:02 -0400

    update 2048

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,28 @@
-#include <SFML/graphics.hpp>
+#include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::Window window(sf::VideoMode({800, 600}), "My window");
+sf::RenderWindow CreateWindow() {
+    sf::RenderWindow window(sf::VideoMode({768, 768}), "2048", sf::Style::Close | sf::Style::Titlebar);
+    return window;
+}
+int main() {
+    sf::RenderWindow window = CreateWindow();
+    sf::Texture texture;
+    if (!texture.loadFromFile("assets/gameBoard.png")) {
+        return 1;
+    }
+    sf::Sprite sprite(texture);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
+
+    while (window.isOpen()) {
+        window.setFramerateLimit(60);
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
+            }
+
+            window.clear(sf::Color::White);
+            window.draw(sprite);
+            window.display();
         }
     }
 }
